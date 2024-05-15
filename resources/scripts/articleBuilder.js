fc862ebbb284c296a402a062c5081bf4196741fb
Chase Wagler <chase@cwagler.com>
2024-05-15 17:25:45 -0600

    Addded ids to all article headers for future linking

--- resources/scripts/articleBuilder.js
+++ resources/scripts/articleBuilder.js
@@ -1,8 +1,6 @@
 // ---------- Set the reading time duration ----------
 let readTime = document.getElementById("article-content").innerHTML.trim().split(" ").length / 225; //the actual reading average is closer to 250 according to google, but we want to give an underestimate
-let articleContents = document.getElementById("read-time");
-
-articleContents.textContent = (Math.max(1, Math.round(readTime)) + "min read time");
+document.getElementById("read-time").textContent = (Math.max(1, Math.round(readTime)) + "min read time");
 
 // ---------- Add "back to top" button ----------
 article = document.getElementsByTagName("article")[0];
@@ -13,7 +11,13 @@ if (article.offsetHeight > 2 * window.innerHeight) {
 
 	var backToTopElement = document.createElement("a");
 	backToTopElement.appendChild(backToTopButton);
-	backToTopElement.setAttribute('href', "#top");
+	backToTopElement.setAttribute("href", "#top");
 
 	article.appendChild(backToTopElement);
+}
+
+// ---------- Add links to each header ----------
+headers = document.getElementsByTagName("h2");
+for (let i = 0; i < headers.length; i++) {
+	headers[i].setAttribute("id", (headers[i].textContent.replaceAll(" ", "-")).toLowerCase());
 }
