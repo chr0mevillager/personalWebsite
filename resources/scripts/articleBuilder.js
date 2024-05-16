cb0f2f8c9d166d36b44b88e3793e73e0fe992fa0
Chase Wagler <chase@cwagler.com>
2024-05-16 00:29:05 -0600

    Added dropdown to link to headers in articles

--- resources/scripts/articleBuilder.js
+++ resources/scripts/articleBuilder.js
@@ -1,3 +1,10 @@
+/*
+
+This file relies on the buttonBuilder script to create buttons and add elements to the dropdown.
+Therefore, this script should always be run before the buttonBuilder.
+
+*/
+
 // ---------- Set the reading time duration ----------
 let readTime = document.getElementById("article-content").innerHTML.trim().split(" ").length / 225; //the actual reading average is closer to 250 according to google, but we want to give an underestimate
 document.getElementById("read-time").textContent = (Math.max(1, Math.round(readTime)) + "min read time");
@@ -16,8 +23,19 @@ if (article.offsetHeight > 2 * window.innerHeight) {
 	article.appendChild(backToTopElement);
 }
 
-// ---------- Add links to each header ----------
+// ---------- Add dropdown link to each header ----------
 headers = document.getElementsByTagName("h2");
-for (let i = 0; i < headers.length; i++) {
-	headers[i].setAttribute("id", (headers[i].textContent.replaceAll(" ", "-")).toLowerCase());
+headerList = document.getElementsByClassName("header-list");
+
+for (var j = 0; j < headerList.length; j++) {
+	for (let i = 0; i < Math.min(headers.length, 10 /* dropdowns only support up to 10 items, so we have a hard limit in case we ever reach >10 headers (should never happen in reality) */); i++) {
+		var headerId = headers[i].textContent.replaceAll(" ", "-").toLowerCase();
+		headers[i].setAttribute("id", headerId);
+		
+		var li = document.createElement("li");
+		li.appendChild(document.createElement("a"));
+		li.firstChild.setAttribute("href", "#" + headerId);
+		li.firstChild.textContent = headers[i].textContent;
+		headerList[j].appendChild(li);
+	}
 }
